83882f6fe98f7bd90a2651a6a74a8dffb443b7c2
mrkchoi <kennethichoi@gmail.com>
2019-05-17 15:39:03 -0700

    Add PTN#initialize, #parent=, #add_child, #remove_child

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -1,2 +1,46 @@
 class PolyTreeNode
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(parent)
+    @parent = parent
+
+    if !@parent.nil? && !@parent.children.include?(self) 
+       @parent.add_child(self)
+    else
+       @parent.remove_child(self)
+    end
+  end
+  
+  def add_child(child)
+    @children << child
+  end
+
+  def remove_child(child)
+    
+  end
+
+  # def inspect
+  #   return "Node: #{self.value}, Children: #{self.children}, Parent: #{self.parent}"
+  # end
+
+  attr_reader :parent, :value, :children
 end
+
+n1 = PolyTreeNode.new(1)
+n2 = PolyTreeNode.new(2)
+n3 = PolyTreeNode.new(3)
+
+n3.parent=(n2)
+
+
+# n2.parent=(n1)
+# n3.parent=(n1)
+
+# n3.parent=nil
+# n3.parent=n2
+
+
